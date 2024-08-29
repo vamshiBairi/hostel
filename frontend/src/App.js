cc8b54ff6e1cb66b8c17207afb8278e394f74688
vishmisc1 <vamshibhairi123@gmail.com>
2024-08-30 00:47:42 +0530

    done yaaar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,19 @@
 import React from 'react';
 import { Route,Routes} from 'react-router-dom';
-import AdminDashboard from './components/AdminDashboard';
-// import adminlogin from './Landing/adminlogin'
-
 import './index.css';
-import StudentDashboard from './components/StudentDashboard';
-// import Login from './components/Login';
-// import AdminDashboard from './components/AdminDashboard';
-import MakeAnnouncement from './components/admin/MakeAnnouncements';
-import AddFoodItem from './components/admin/AddFoodItem';
-import AddStudent from './components/admin/AddStudent';
-import Navbar from './components/Navbar';
-import ViewComplaints from './components/admin/ViewComplaints';
-import ViewStudents from './components/admin/ViewStudents';
-import SelectMeal from './components/student/SelectMeal';
-import ViewAnnouncements from './components/student/ViewAnnouncements';
 import LoginPage from './Landing/Login';
-// import Slides from './Landing/';
-// import LoginPage from './Landing/Login';
 import AdminLogin from './Landing/adminlogin';
+import AdminDashboard from './components/AdminDashboard';
+import StudentDashboard from './components/StudentDashboard';
 function App() {
   return (
-    
     <div className="App">
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
         <Route path="/admin/*" element={<AdminDashboard />} /> 
         <Route path="/admin-login" element={<AdminLogin/>} /> 
         <Route path="/student-dashboard/*" element={<StudentDashboard />}/>
-        {/* Add other routes here */}
       </Routes>
-      {/* <StudentDashboard/> */}
-      {/* <LoginPage/> */}
-      {/* <div className="container mt-3">
-        <Switch>
-          <Route path="/admin" component={AdminDashboard} />
-          <Route path="/student" component={StudentDashboard} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </div> */}
     </div>
   );
 }
