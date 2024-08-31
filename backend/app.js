05ee87b57edc7f2dad0e6afa20f0dd89eaeac567
vishmisc1 <vamshibhairi123@gmail.com>
2024-08-31 17:25:02 +0530

    Deploy

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin:[""],
+    origin:["https://goodday-hostels.vercel.app"],
     methods:["POST","GET"],
     credentials:true
   }
