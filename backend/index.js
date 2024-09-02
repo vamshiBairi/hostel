6fdcbf50464316363f66e6c11eb35089370fdcac
vishmisc1 <vamshibhairi123@gmail.com>
2024-09-02 10:17:56 +0530

    backend vercel

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://goodday-hostels.vercel.app', // Allow only your frontend's domain
-  methods: 'GET,POST,PUT,DELETE', // Specify the allowed methods
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+  origin: 'https://goodday-hostels.vercel.app', 
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true 
 }));
 
 console.log("Backend started");
@@ -25,8 +25,9 @@ const studentRoutes = require('./routes/student');
 app.use('/admin', adminRoutes);
 app.use('/student', studentRoutes);
 
-// Start the Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
